ce5f0c98a60bb574f94b1216d10b9fafa45cfb55
Russell Jones <jonesrussell42@gmail.com>
2020-05-28 19:57:22 -0400

    WIP

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -3,7 +3,3 @@
 chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
-
-chrome.browserAction.setBadgeText({text: '\'HAllo'});
-
-console.log('\'HAllo \'HAllo! Event Page for Browser Action');
--- app/scripts.babel/chromereload.js
+++ app/scripts.babel/chromereload.js
@@ -1,35 +1,41 @@
-'use strict';
+"use strict";
 
 // Reload client for Chrome Apps & Extensions.
 // The reload client has a compatibility with livereload.
 // WARNING: only supports reload command.
 
-const LIVERELOAD_HOST = 'localhost:';
+const LIVERELOAD_HOST = "localhost";
 const LIVERELOAD_PORT = 35729;
-const connection = new WebSocket('ws://' + LIVERELOAD_HOST + LIVERELOAD_PORT + '/livereload');
+const LIVERELOAD = `ws://{LIVERELOAD_HOST}:{LIVERELOAD_PORT}/livereload`;
+const connection = new WebSocket(LIVERELOAD);
 
-var lastReload = false;
+let lastReload = false;
 
-chrome.runtime.onInstalled.addListener(function(details) {
+chrome.runtime.onInstalled.addListener(function (details) {
   lastReload = Date.now();
-});    
+});
 
-connection.onerror = error => {
-  console.log('reload connection got error:', error);
+connection.onerror = (error) => {
+  console.log("reload connection got error:", error);
 };
 
-connection.onmessage = e => {
+const ONE_MINUTE_MS = 60000;
+
+connection.onmessage = (e) => {
   if (e.data) {
     const data = JSON.parse(e.data);
-    if (data && data.command === 'reload') {
-      var currentTime = Date.now();
-      if (lastReload && currentTime - lastReload > 60000) {
-	// don't reload more than once a minute
-	chrome.runtime.reload();
-	chrome.developerPrivate.reload(chrome.runtime.id, 
-				       {failQuietly: true});
+
+    if (data && data.command === "reload") {
+      let currentTime = Date.now();
+      let sinceLastReload = currentTime - lastReload;
+      let overOneMinute = sinceLastReload > ONE_MINUTE_MS;
+
+      if (lastReload && overOneMinute) {
+        chrome.runtime.reload();
+        chrome.developerPrivate.reload(chrome.runtime.id, {
+          failQuietly: true,
+        });
       }
     }
   }
 };
-
