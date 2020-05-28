8adeba77d9a1053ebcf1cc1699fed1ac7c493ea4
Russell Jones <jonesrussell42@gmail.com>
2020-05-28 00:17:00 -0400

    WIP

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -4,6 +4,6 @@ chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
+chrome.browserAction.setBadgeText({text: '\'HAllo'});
 
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+console.log('\'HAllo \'HAllo! Event Page for Browser Action');
